400fe43a79f76b5a0f8c1bf6a38222931bc99166
DongDong <qltkd5959@gmail.com>
2023-09-11 19:47:29 +0900

    Revert "JSDoc 체험해보기"
    This reverts commit 1dba8fb78a2ec20b49d2a5580cb2d0bb750d8d0a.

--- project/app.js
+++ project/app.js
@@ -41,10 +41,6 @@ let isDeathLoading = false;
 let isRecoveredLoading = false;
 
 // api
-/**
- *
- * @returns {Promise}
- */
 function fetchCovidSummary() {
   const url = "https://api.covid19api.com/summary";
   return axios.get(url);
